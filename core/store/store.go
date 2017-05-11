761b71113bd2a2bf0a2ecd14de4a4a4ad4a37152
Gerard Abelló Serras <gerardabello@gmail.com>
2017-05-11 20:34:33 +0200

    Reorganize store definition

--- core/store/store.go
+++ core/store/store.go
@@ -13,16 +13,21 @@ type Store interface {
 	Close()
 	GetResults() ([]structs.Result, error)
 	GetResultsByBatch(string) ([]structs.Result, error)
-	GetBatchs() ([]structs.BatchInfo, error)
 	GetResult(string) (structs.Result, error)
 	GetLastResult(projectID, branch, target, browser string) (structs.Result, error)
+	StoreResult(structs.Result) error
+
+	GetBatchs() ([]structs.BatchInfo, error)
+
 	GetMask(string) ([]image.Rectangle, error)
 	StoreMask(masks []image.Rectangle) (string, error)
-	StoreResult(structs.Result) error
+
 	GetImage(string) (image.Image, error)
 	StoreImage(image.Image) (string, error)
+
 	GetBaseImageID(projectID, branch, target, browser string) (string, error)
 	SetBaseImageID(baseImageID, projectID, branch, target, browser string) error
+
 	GetBaseMaskID(projectID, branch, target, browser string) (string, error)
 	SetBaseMaskID(baseImageID, projectID, branch, target, browser string) error
 }
